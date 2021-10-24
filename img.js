d830494771614fb24958ded4243598dc7c9760f7
alphakys <gys1120@naver.com>
2021-10-24 11:31:25 +0900

    파일까지 끝

--- img.js
+++ img.js
@@ -12,12 +12,24 @@ var server = http.createServer(function(req,res){
     var path = resource.pathname.split('/');
     //console.log(path[1]);
     if(path[1] =='images'){
-        var images = path[2];
-        console.log(mime.getType());
+        var images = 'images/'+path[2];
+        console.log(images);
+        console.log(mime.getType(images));
+    
+        fs.readFile(images, function(err, data){
+            if(err){
+                res.writeHead(500, {'Content-Type':'text/html'});
+                res.end('error');
+                console.log(err);
+            }else{
+                res.end(data);
+            }
+        });
+    }else{
+        res.writeHead(404, {'Content-Type':'text/html'});
+        res.end('404 not found');
     }
 
-
-    res.end();
 });
 
 
